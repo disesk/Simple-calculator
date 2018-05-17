f91e40741a2b5fc4bb45586d99b3ac9724173614
Dima <skripka.d.s@gmail.com>
2018-05-17 15:20:09 +0300

    Add files

---
+++ js/common.js
@@ -0,0 +1,50 @@
+plus.onclick = function () {
+	var num1 = document.getElementById("numOne").value;
+	num1 = parseFloat(num1);
+	var num2 = document.getElementById("numTwo").value;
+	num2 = parseFloat(num2);
+	var result = num1 + num2;
+	if (isFinite(result)) {
+		document.getElementById("result").innerHTML = result;
+	} else {
+		alert('Введены некорректные данные');
+	}
+};
+ 
+minus.onclick = function () {
+	var num1 = document.getElementById("numOne").value;
+	num1 = parseFloat(num1);
+	var num2 = document.getElementById("numTwo").value;
+	num2 = parseFloat(num2);
+	var result = num1 - num2;
+	if (isFinite(result)) {
+		document.getElementById("result").innerHTML = result;
+	} else {
+		alert('Введены некорректные данные');
+	}
+};
+
+divide.onclick = function () {
+	var num1 = document.getElementById("numOne").value;
+	num1 = parseFloat(num1);
+	var num2 = document.getElementById("numTwo").value;
+	num2 = parseFloat(num2);
+	var result = num1 / num2;
+	if (isFinite(result)) {
+		document.getElementById("result").innerHTML = result;
+	} else {
+		alert('Введены некорректные данные');
+	}
+};
+multiply.onclick = function () {
+	var num1 = document.getElementById("numOne").value;
+	num1 = parseFloat(num1);
+	var num2 = document.getElementById("numTwo").value;
+	num2 = parseFloat(num2);
+	var result = num1 * num2;
+	if (isFinite(result)) {
+		document.getElementById("result").innerHTML = result;
+	} else {
+		alert('Введены некорректные данные');
+	}
+};
